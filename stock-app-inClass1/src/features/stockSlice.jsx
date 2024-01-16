c61917ce464c9612076de5781f0e44e651d6c598
Mucahit <mucahit.kartal2015@gmail.com>
2024-01-16 19:28:25 +0300

    Update UI and add new features

--- stock-app-inClass1/src/features/stockSlice.jsx
+++ stock-app-inClass1/src/features/stockSlice.jsx
@@ -1,7 +1,47 @@
-import React from "react"
+import { createSlice } from "@reduxjs/toolkit"
 
-const stockSlice = () => {
-  return <div>stockSlice</div>
+const initialState = {
+  products: [],
+  purchases: [],
+  brands: [],
+  sales: [],
+  categories: [],
+  loading: false,
+  error: false,
+  firms: [],
 }
 
-export default stockSlice
+const stockSlice = createSlice({
+  name: "stock",
+  initialState,
+  reducers: {
+    fetchStart: (state) => {
+      state.loading = true
+    },
+    // getFirmsSuccess: (state, { payload }) => {
+    //   state.firms = payload
+    //   state.loading = false
+    // },
+    // getSalesSuccess: (state, { payload }) => {
+    //   state.sales = payload
+    //   state.loading = false
+    // },
+    // getStockSuccess: (state, { payload: { url, apiData } }) => {
+    //   state[url] = apiData
+    //   state.loading = false
+    // },
+    getStockSuccess: (state, action) => {
+      state[action.payload.url] = action.payload.apiData
+      state.loading = false
+    },
+
+    fetchFail: (state) => {
+      state.loading = false
+      state.error = true
+    },
+  },
+})
+
+export const { fetchStart, getStockSuccess, fetchFail } = stockSlice.actions
+
+export default stockSlice.reducer
